9c1b06ca751bbd1bb5dc6d56686f7459498366ee
cpereira7 <cpereira7@users.noreply.github.com>
2025-08-11 01:14:37 +0100

    feat(datasync): add trigger function for data change notifications
    - Created a new trigger function `notify_on_data_change` that sends a JSON notification via `pg_notify` whenever there is an insert, update, or delete operation on the `EmergencyEvents` table.
    - The function is designed to run as an AFTER trigger and raises an exception if used otherwise.
    - Added a trigger `on_data_change` to the `EmergencyEvents` table to utilize this function.

--- infrastructure/init.sql
+++ infrastructure/init.sql
@@ -17,3 +17,36 @@ CREATE TABLE EmergencyEvents (
     timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE FUNCTION public.notify_on_data_change()
+  RETURNS trigger
+  LANGUAGE 'plpgsql'
+AS $BODY$ 
+DECLARE 
+  data JSON;
+  notification JSON;
+BEGIN
+
+IF TG_WHEN <> 'AFTER' THEN
+  RAISE EXCEPTION 'public.notify_on_data_change() may only run as an AFTER trigger';
+END IF;
+  IF (TG_OP = 'DELETE') THEN
+    data = row_to_json(OLD);
+  ELSE
+    data = row_to_json(NEW);
+  END IF;
+
+notification = json_build_object(
+  'table',TG_TABLE_NAME,
+  'action', TG_OP,
+  'data', data);  
+            
+    PERFORM pg_notify('datachange', notification::TEXT);
+  RETURN NEW;
+END
+$BODY$;
+
+CREATE TRIGGER on_data_change
+  AFTER INSERT OR DELETE OR UPDATE 
+  ON public.EmergencyEvents
+  FOR EACH ROW
+  EXECUTE PROCEDURE public.notify_on_data_change();
