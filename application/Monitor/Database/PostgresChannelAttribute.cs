46dfb9909c927de0e693866f6f4aff78f22c56ca
cpereira7 <cpereira7@users.noreply.github.com>
2025-08-17 00:06:01 +0100

    feat(monitor): Add PostgresChannelAttribute for monitoring
    - Creates a new file `PostgresChannelAttribute.cs`
    - Implements a custom attribute to assign PostgreSQL channels.

---
+++ application/Monitor/Database/PostgresChannelAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Monitor.Database;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public sealed class PostgresChannelAttribute : Attribute
+{
+    public string ChannelName { get; }
+
+    public PostgresChannelAttribute(string channelName)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(channelName);
+
+        ChannelName = channelName;
+    }
+}
